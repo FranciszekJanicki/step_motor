129e2a201f89cd2824d24a20ad168df47bd5e934
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-06-25 19:58:41 +0200

    changes

--- step_motor_config.h
+++ step_motor_config.h
@@ -9,41 +9,41 @@ typedef float float32_t;
 #endif // FLOAT32
 
 typedef enum {
-    STEP_MOTOR_ERR_OK = 0,
-    STEP_MOTOR_ERR_FAIL = 1 << 0,
-    STEP_MOTOR_ERR_NULL = 1 << 1,
+  STEP_MOTOR_ERR_OK = 0,
+  STEP_MOTOR_ERR_FAIL = 1 << 0,
+  STEP_MOTOR_ERR_NULL = 1 << 1,
 } step_motor_err_t;
 
 typedef enum {
-    STEP_MOTOR_DIRECTION_FORWARD,
-    STEP_MOTOR_DIRECTION_BACKWARD,
-    STEP_MOTOR_DIRECTION_STOP,
+  STEP_MOTOR_DIRECTION_FORWARD,
+  STEP_MOTOR_DIRECTION_BACKWARD,
+  STEP_MOTOR_DIRECTION_STOP,
 } step_motor_direction_t;
 
 typedef struct {
-    uint32_t frequency;
-    int64_t step_count;
-    float32_t prev_position;
-    float32_t prev_speed;
-    step_motor_direction_t direction;
+  uint32_t frequency;
+  int64_t step_count;
+  float32_t prev_position;
+  float32_t prev_speed;
+  step_motor_direction_t direction;
 } step_motor_state_t;
 
 typedef struct {
-    float32_t min_position;
-    float32_t max_position;
-    float32_t min_speed;
-    float32_t max_speed;
-    float32_t min_acceleration;
-    float32_t max_acceleration;
-    float32_t step_change;
+  float32_t min_position;
+  float32_t max_position;
+  float32_t min_speed;
+  float32_t max_speed;
+  float32_t min_acceleration;
+  float32_t max_acceleration;
+  float32_t step_change;
 } step_motor_config_t;
 
 typedef struct {
-    void* device_user;
-    step_motor_err_t (*device_init)(void*);
-    step_motor_err_t (*device_deinit)(void*);
-    step_motor_err_t (*device_set_frequency)(void*, uint32_t);
-    step_motor_err_t (*device_set_direction)(void*, step_motor_direction_t);
+  void *device_user;
+  step_motor_err_t (*device_initialize)(void *);
+  step_motor_err_t (*device_deinitialize)(void *);
+  step_motor_err_t (*device_set_frequency)(void *, uint32_t);
+  step_motor_err_t (*device_set_direction)(void *, step_motor_direction_t);
 } step_motor_interface_t;
 
 #endif // STEP_MOTOR_STEP_MOTOR_CONFIG_H
